45441ea6e3cef6121dce11ac9be6036d1f66cea4
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 11:33:09 +0400

    added get by id in controller

--- src/main/java/com/SchoolManagment/SchoolManagment/Course/CourseController.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Course/CourseController.java
@@ -2,6 +2,7 @@ package com.SchoolManagment.SchoolManagment.Course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,9 @@ public class CourseController {
     @GetMapping("/getAll")
     public List<Course> getAllCourses() {
         return courseService.getAllCourses();
+    }
+    @GetMapping("{id}")
+    public Course getById(@PathVariable Long courseId) {
+        return courseService.findById(courseId);
     }
 }
