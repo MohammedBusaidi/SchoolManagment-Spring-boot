fc9dd4476d285efb537a623c514a2c7e2723cb02
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 11:25:46 +0400

    added add class in controller

--- src/main/java/com/SchoolManagment/SchoolManagment/Class/ClassController.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Class/ClassController.java
@@ -1,10 +1,7 @@
 package com.SchoolManagment.SchoolManagment.Class;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,9 @@ public class ClassController {
     @GetMapping("{id}")
     public Class getById(@PathVariable long classId) {
         return classService.findById(classId);
+    }
+    @PostMapping("/addClass")
+    public Class createClass(@RequestBody Class cls) {
+        return classService.addeClass(cls);
     }
 }
