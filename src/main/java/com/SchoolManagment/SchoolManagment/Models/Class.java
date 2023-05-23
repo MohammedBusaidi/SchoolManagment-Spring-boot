5072094959e78dd1d6c4650b30867bc8546e72bd
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 10:12:51 +0400

    createed student class

--- src/main/java/com/SchoolManagment/SchoolManagment/Models/Class.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Models/Class.java
@@ -1,4 +1,16 @@
 package com.SchoolManagment.SchoolManagment.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Class {
 }
--- src/main/java/com/SchoolManagment/SchoolManagment/Models/Student.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Models/Student.java
@@ -1,4 +1,27 @@
 package com.SchoolManagment.SchoolManagment.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+    private String studentName;
+    private int age;
+    private String phoneNumber;
+    private String nationality;
+    private String gender;
 }
