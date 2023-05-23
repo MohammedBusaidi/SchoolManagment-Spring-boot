28a2c849cbc18e4c6fac8fd36851ea425bce0716
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 11:34:33 +0400

    added add course in controller

--- src/main/java/com/SchoolManagment/SchoolManagment/Course/CourseController.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Course/CourseController.java
@@ -1,10 +1,7 @@
 package com.SchoolManagment.SchoolManagment.Course;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,9 @@ public class CourseController {
     @GetMapping("{id}")
     public Course getById(@PathVariable Long courseId) {
         return courseService.findById(courseId);
+    }
+    @PostMapping("/addCourse")
+    public Course addCourse(@RequestBody Course course) {
+        return courseService.addCourse(course);
     }
 }
