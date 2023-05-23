b71cd8b8dcaa1bb3de12add3b5800a6a23bd417e
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 11:47:14 +0400

    added get by id in controller

--- src/main/java/com/SchoolManagment/SchoolManagment/Teacher/TeacherController.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Teacher/TeacherController.java
@@ -1,6 +1,8 @@
 package com.SchoolManagment.SchoolManagment.Teacher;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,12 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
+    @GetMapping("/getAll")
     public List<Teacher> getAllTeachers() {
         return teacherService.getAllTeachers();
+    }
+    @GetMapping("{id}")
+    public Teacher getById(@PathVariable("id") Long teacherId) {
+        return teacherService.findById(teacherId);
     }
 }
