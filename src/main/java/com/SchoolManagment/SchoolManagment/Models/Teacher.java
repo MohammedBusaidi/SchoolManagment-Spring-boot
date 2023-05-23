a2bb9c5c0baaac2b01694669ea33f2c856c4acb2
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 10:14:05 +0400

    created teacher class

--- src/main/java/com/SchoolManagment/SchoolManagment/Models/Teacher.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/Models/Teacher.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
@@ -13,4 +16,12 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 @Entity
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long teacherId;
+    private String teacherName;
+    private int age;
+    private String phoneNumber;
+    private String nationality;
+    private String gender;
 }
