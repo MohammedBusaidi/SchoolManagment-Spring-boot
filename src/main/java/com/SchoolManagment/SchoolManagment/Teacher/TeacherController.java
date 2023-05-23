83e33146af3417dcf4b5afd29e74a0586144be36
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 11:45:50 +0400

    created teacher controller and added get all teachers

---
+++ src/main/java/com/SchoolManagment/SchoolManagment/Teacher/TeacherController.java
@@ -0,0 +1,18 @@
+package com.SchoolManagment.SchoolManagment.Teacher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/teacher")
+public class TeacherController {
+    @Autowired
+    TeacherService teacherService;
+
+    public List<Teacher> getAllTeachers() {
+        return teacherService.getAllTeachers();
+    }
+}
