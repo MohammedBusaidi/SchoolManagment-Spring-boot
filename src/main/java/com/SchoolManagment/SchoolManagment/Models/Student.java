2d6aa0fe61036134ff87475d01d8d91745691221
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 10:04:47 +0400

    created the base

---
+++ src/main/java/com/SchoolManagment/SchoolManagment/Models/Student.java
@@ -0,0 +1,4 @@
+package com.SchoolManagment.SchoolManagment.Models;
+
+public class Student {
+}
---
+++ src/test/java/com/SchoolManagment/SchoolManagment/SchoolManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SchoolManagment.SchoolManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
