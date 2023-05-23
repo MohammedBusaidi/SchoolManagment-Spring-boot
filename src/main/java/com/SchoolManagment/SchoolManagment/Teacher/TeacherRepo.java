feea9b3f142ccea0c07e878fa9ec810e6dd62685
Mohammed <moody.busaidi99@gmail.com>
2023-05-23 10:40:03 +0400

    teacher repo

---
+++ src/main/java/com/SchoolManagment/SchoolManagment/Teacher/TeacherRepo.java
@@ -0,0 +1,6 @@
+package com.SchoolManagment.SchoolManagment.Teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepo extends JpaRepository<Teacher,Long> {
+}
