6f5862824fa5ea372ec2ec7eada4c9a69026fb4a
Mohammed <moody.busaidi99@gmail.com>
2023-05-24 13:32:35 +0400

    created student course service and added get one method

--- src/main/java/com/SchoolManagment/SchoolManagment/StudentCourse/StudentCourseService.java
+++ src/main/java/com/SchoolManagment/SchoolManagment/StudentCourse/StudentCourseService.java
@@ -1,8 +1,15 @@
 package com.SchoolManagment.SchoolManagment.StudentCourse;
 
+import com.SchoolManagment.SchoolManagment.Student.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentCourseService {
+    @Autowired
+    StudentCourseRepo studentCourseRepo;
 
+    public StudentCourse getOne(Long studentCourseId) {
+        return studentCourseRepo.getOne(studentCourseId);
+    }
 }
